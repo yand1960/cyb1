dd146c970e334fe19e22e6b13769625efaaf213e
yand1960 <yand1960@gmail.com>
2021-12-12 14:12:01 +0300

    YA

--- register.php
+++ register.php
@@ -15,17 +15,20 @@
 
             include(getenv("MYAPP_CONFIG"));
             $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
-            //Нудная, но необходимая процедура передачи параметров 
-            //в sql выражение, что гарантирует защиту от инжекции sql
+            // Нудная, но необходимая процедура передачи параметров 
+            // в sql выражение, что гарантирует защиту от инжекции sql
             $statement = mysqli_prepare($conn, $sql);
             mysqli_stmt_bind_param($statement,"ss",$user,$hash);
+            // Часто применяемая в PHP конструкции "... or die..." 
+            // позволяет прекратить исполнение кода, если первая часть 
+            // выражения вернула что-то "нехорошее":
             mysqli_stmt_execute($statement) or die(mysqli_error($conn));
             mysqli_close($conn);
             echo('<meta http-equiv="refresh" content="2; URL=login.php">');
             die("Новый пользователь $user зарегистрирован");
         }
         
-        ?>
+    ?>
     </body>
         <h1>Register, please!</h1>
         <form method="post">
